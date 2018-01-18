122c7573d3dfd094c48cefc7da9342164e14d883
wupeng <wpjsnj163@163.com>
2018-01-18 20:35:49 +0800

    序列化更新pickle方式

--- myutils/objectserializeutils.py
+++ myutils/objectserializeutils.py
@@ -11,8 +11,54 @@ import json
 
 
 class SerializeObject(object):
-    def serialize(self):
-        pass
+    @staticmethod
+    def serialize(clsobj, serializetype='pickle'):
+        ''''''
+        
+        def pickleserialize(clsobjpickle):
+            return pickle.dumps(clsobjpickle)
+        
+        def jsonserialize(clsobjjson):
+            print clsobjjson.__dict
+            return json.dumps(clsobjjson.__dict)
+        
+        switch = {
+            "pickle": pickleserialize,
+            "json": jsonserialize
+        }
+        
+        try:
+            return switch[serializetype](clsobj)
+        except KeyError as e:
+            raise e
     
-    def unserialize(self):
+    @staticmethod
+    def unserialize(serializetype='pickle'):
         pass
+
+
+class TObj(object):
+    def __init__(self):
+        self._a = 1
+    
+    @property
+    def a(self):
+        print self._a
+
+
+if __name__ == '__main__':
+    import pickle
+    
+    a = TObj()
+    # bytedata = pickle.dumps(a)
+    bytedata = SerializeObject.serialize(a)
+    print bytedata
+    print ">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<"
+    b = pickle.loads(bytedata)
+    b.a
+    
+    jsonsdata = SerializeObject.serialize(a, serializetype='json')
+    print jsonsdata
+    print ">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<"
+    c = json.loads(jsonsdata)
+    c.a
