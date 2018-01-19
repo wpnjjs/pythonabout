d4e3a3f851467e4d43167a3aa23689beee84df0c
wupeng <wpjsnj163@163.com>
2018-01-19 13:35:11 +0800

    序列化模块编写完成

--- myutils/objectserializeutils.py
+++ myutils/objectserializeutils.py
@@ -13,18 +13,16 @@ import json
 class SerializeObject(object):
     @staticmethod
     def serialize(clsobj, serializetype='pickle'):
-        ''''''
-        
-        def pickleserialize(clsobjpickle):
-            return pickle.dumps(clsobjpickle)
-        
-        def jsonserialize(clsobjjson):
-            print clsobjjson.__dict
-            return json.dumps(clsobjjson.__dict)
+        '''
+        将对象序列化，pickle模块序列化或json模块序列化
+        :param clsobj: 需要序列化的对象
+        :param serializetype: 序列化类型，pickle，使用pickle方式序列化；json，使用json方式序列化
+        :return: 序列化结果
+        '''
         
         switch = {
-            "pickle": pickleserialize,
-            "json": jsonserialize
+            "pickle": lambda obj: pickle.dumps(obj),
+            "json": lambda obj: json.dumps(obj.__dict__)
         }
         
         try:
@@ -33,32 +31,49 @@ class SerializeObject(object):
             raise e
     
     @staticmethod
-    def unserialize(serializetype='pickle'):
-        pass
-
-
-class TObj(object):
-    def __init__(self):
-        self._a = 1
-    
-    @property
-    def a(self):
-        print self._a
+    def unserialize(datastr, serializetype='pickle'):
+        '''
+        反序列化
+        :param datastr: 序列化字符串
+        :param serializetype: 何种方式反序列化，一定要对应
+        :return: 反序列化结果，pickle方式反序列化为对应类型，json方式返回字典对象，需要自己变为对应的对象
+        '''
+        
+        switch = {
+            "pickle": lambda s: pickle.loads(s),
+            "json": lambda s: json.loads(s)
+        }
+        
+        try:
+            return switch[serializetype](datastr)
+        except KeyError as e:
+            raise e
 
 
 if __name__ == '__main__':
-    import pickle
+    '''
+    eg.
+    '''
+    
+    
+    class TObj(object):
+        def __init__(self):
+            self._a = 1
+        
+        @property
+        def a(self):
+            print self._a
+    
     
     a = TObj()
-    # bytedata = pickle.dumps(a)
     bytedata = SerializeObject.serialize(a)
-    print bytedata
+    print bytedata, a.__dict__
     print ">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<"
-    b = pickle.loads(bytedata)
+    b = SerializeObject.unserialize(bytedata)
     b.a
     
-    jsonsdata = SerializeObject.serialize(a, serializetype='json')
+    jsonsdata = SerializeObject.serialize(b, serializetype='json')
     print jsonsdata
     print ">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<"
-    c = json.loads(jsonsdata)
-    c.a
+    c = SerializeObject.unserialize(jsonsdata, serializetype='json')
+    print c
